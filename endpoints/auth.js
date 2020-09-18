b1bf5808bc4b9da407ac7c23250e7bddad4ceaf3
Ignacio <ignaciocampo.r@gmail.com>
2020-09-18 02:19:57 -0300

    folders and auth endpoint

---
+++ endpoints/auth.js
@@ -0,0 +1,83 @@
+const router = require('express').Router();
+// const bcrypt = require('bcryptjs')
+// const jwt = require('jsonwebtoken')
+const Users = require('./auth-model')
+
+
+router.post('/register', async (req, res, next) => {
+    try{
+        const {name} = req.body
+        const username = await Users.findBy({name})
+  
+        if (username){
+          return res.status(409).json({
+              message: "User is already taken"
+          })
+        }
+        res.status(201).json(await Users.add(req.body))
+
+    }
+    catch(err){
+      next(err)
+    }
+});
+
+
+router.post("/login", async (req, res, next) => {
+	try {
+		const { username, password } = req.body
+		const user = await Users.findBy({ username }).first()
+		
+		if (!user) {
+			return res.status(401).json({
+				message: "Invalid Credentials",
+			})
+		}
+		restrict()
+
+		res.json({
+			message: `Welcome ${user.username}!`,
+		})
+	} catch(err) {
+		next(err)
+	}
+})
+
+// router.post('/login', async (req, res, next) => {
+//    console.log('this is the request', req)
+//    const authError = {
+//       message: "Invalid Credentials"
+//    }
+//    const {name} = req.body  
+//    try{
+//       const userData = await Users.findBy({name})
+
+//         if (!userData){
+//           return res.status(401).json(authError)
+//       }
+//       console.log(req.body.password, userData.password)
+//       const passwordValid = await bcrypt.compare(req.body.password, userData.password)
+//       console.log(passwordValid)
+//         if (!passwordValid){
+//             return res.status(401).json(authError)
+//         }
+
+//       const tokenPayload = {
+//           userId: userData.id,
+//           userName: userData.name,
+//           userRole: userData.role
+//       }
+      
+//       const token = jwt.sign(tokenPayload, process.env.JWT_SECRET) 
+//       res.json({
+//           message: `Welcome ${userData.name}!`,
+//           token: token
+//       })
+//    }
+//    catch(err){
+//      next(err)
+//    }
+// });
+
+
+module.exports = router;
