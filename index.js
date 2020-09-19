a7820ea49e15308600747d65ec8da1755e704645
Mustafa Hassan <60632959+MustafaCajib@users.noreply.github.com>
2020-09-18 17:39:36 -0700

    welcome api

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const server = require("./api/server.js");
 
 // const PORT = process.env.PORT;
 
-// server.get("/", (req, res) => {
-//   res.status(200).json({
-//     message: "Welcome to API!!",
-//   });
-// });
+server.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to API!!",
+  });
+});
 // server.listen(PORT, () => {
 //   console.log(`\n** Server listening on port ${PORT} **\n`);
 // });
