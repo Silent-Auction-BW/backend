b622d5915f2abc7e66fb636e94684a2116c3456f
Ignacio <ignaciocampo.r@gmail.com>
2020-09-21 21:27:41 -0300

    username as name should be testing

--- migrations/20200918191222_auction_table.js
+++ migrations/20200918191222_auction_table.js
@@ -2,13 +2,13 @@
 exports.up = async function(knex) {
     await knex.schema.createTable("sellers", (table) => {
         table.increments("seller_id")
-        table.string("name").unique().notNullable()
+        table.string("username").unique().notNullable()
         table.string("password").notNullable()
     })
     
     await knex.schema.createTable("bidders", (table) => {
         table.increments("bidder_id")
-        table.string("name").unique().notNullable()
+        table.string("username").unique().notNullable()
         table.string("password").notNullable()
     })
     await knex.schema.createTable("items", (table) => {
--- models/auth-model.js
+++ models/auth-model.js
@@ -7,11 +7,11 @@ async function add(user) {
 	// user.password = await bcrypt.hash(user.password, 14)
 
 	if(user.role === 'seller'){
-		const [seller_id] = await db("sellers").insert({name: user.name, password: user.password})
+		const [seller_id] = await db("sellers").insert({username: user.username, password: user.password})
 		return findById(seller_id, user.role)
 	}
 	else if (user.role ==='bidder'){
-		const [bidder_id] = await db("bidders").insert({name: user.name, password: user.password})
+		const [bidder_id] = await db("bidders").insert({username: user.username, password: user.password})
         return findById(bidder_id, user.role) 
 	}
 }
@@ -22,12 +22,12 @@ async function findBy(filter) {
         
     const bidder = await db("bidders as b")
         .where(filter)
-        .select("b.bidder_id", "b.name", "b.password")
+        .select("b.bidder_id", "b.username", "b.password")
         .first()
       
     const seller = await db("sellers as s")
         .where(filter)
-        .select("s.seller_id", "s.name", "s.password")
+        .select("s.seller_id", "s.username", "s.password")
         .first()
 
 
@@ -44,12 +44,12 @@ async function findBy(filter) {
 async function findById(bidder_id, seller_id, role) {
 
 	const bidder = await db("bidders")
-		.select("bidder_id", "name")
+		.select("bidder_id", "username")
 		.where({ bidder_id })
 		.first()
 
 	const seller = await db("sellers")
-	.select("seller_id", "name")
+	.select("seller_id", "username")
 	.where({ seller_id })
 	.first()
 
