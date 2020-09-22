08b1b5c328696b81c32892200de1a449e9085b05
Mustafa Hassan <60632959+MustafaCajib@users.noreply.github.com>
2020-09-21 18:08:42 -0700

    added items

--- seeds/items.js
+++ seeds/items.js
@@ -14,7 +14,14 @@ exports.seed = function(knex) {
         description: 'Markup Data Markup Data Markup Data Markup Data Markup Data Markup Data Markup Data Markup Data Markup Data ',
         timer: '15:00 min'
       },
-        // {id: 2, colName: 'rowValue2'},
+        { 
+          item_id : 2,
+          seller_id: 1,
+          name: 'William',
+        image: 'https://images.unsplash.com/photo-1519687774292-8ef26b975fc4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+        price: { bidState: false, price: 100 },
+        description: 'Markup Data Markup Data Markup Data Markup Data Markup Data Markup Data Markup Data Markup Data Markup Data ',
+        timer: '15:00 min'},
         // {id: 3, colName: 'rowValue3'}
       ]);
     });
