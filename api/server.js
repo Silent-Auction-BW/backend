4139be9e5a882e8eba3111cbad87a46c8afb263d
Ignacio <ignaciocampo.r@gmail.com>
2020-09-24 17:04:48 -0300

    payload too large fixing

--- api/server.js
+++ api/server.js
@@ -9,7 +9,9 @@ const server = express();
 
 server.use(helmet());
 server.use(cors());
-server.use(express.json());
+// server.use(express.json());
+server.use(express.json({limit: '50mb'}));
+server.use(express.urlencoded({limit: '50mb'}));
 
 
 server.use('/', authRouter)
